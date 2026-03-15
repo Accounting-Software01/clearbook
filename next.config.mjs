6d52cc20cecce19702317ba132d14857daadc28c
Accounting-Software01 <mshamisu@proton.me>
2026-03-15 06:30:14 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-
-
+const nextConfig = {
+    env: {
+        NEXT_PUBLIC_RECAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+};
 
 export default nextConfig;
